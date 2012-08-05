87a57bb76476009b2e1379144432284c16add9dd
Don <don@don-ThinkPad-T410>
2012-08-05 16:00:18 +0100

    Chapter 7 - user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,39 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessor :password
+	attr_accessible :name, :email, :password, :password_confirmation
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]*\.[a-z]+\z/
 
 
 	validates :name, :presence => true, :length => {:maximum => 50}
 	validates :email, :presence=> true, :format => {:with => email_regex}, :uniqueness => {:case_sensitive=>false}
+	validates :password, :presence=>true, :confirmation => true, :length => {:within => 6..40}
+
+	before_save :encrypt_password
+
+	def has_password?(submitted_password)
+		#compare encrypted password with the encrypted version of submitted password
+	end
+
+	private
+
+		def encrypt_password
+			self.encrypted_password=encrypt(password)
+		end
+
+		def encrypt(string)
+			string.reverse! # Temporary stuff!
+		end
 end
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@ describe User do
   #pending "add some examples to (or delete) #{__FILE__}"
 
   before(:each) do
-  	@attr = {:name => "Example User", :email => "user@example.com"}
+  	@attr = {:name => "Example User", :email => "user@example.com", :password => "foobar", :password_confirmation=>"foobar"}
   end
 
   it "should create new instance given valid attributes" do
@@ -50,17 +50,69 @@ describe User do
     user_with_duplicate_details.should_not be_valid 
   end
 
+  describe "password validations" do
+    it "should require a password" do
+      User.new(@attr.merge(:password=>"", :password_confirmation=>"")).should_not be_valid
+    end
+    it "should require a matching confirmation password" do
+      User.new(@attr.merge(:password_confirmation=>"in_valid")).should_not be_valid 
+    end    
+    it "should reject short passwords" do
+      short = "a" * 5
+      hash = @attr.merge(:password=>short, password_confirmation=>short)
+      User.new(hash).should_not be_valid
+      #User.new(@attr.merge(:password=>short, password_confirmation=>short)).should_not be_valid
+    end
+    it "should reject long passwords" do
+      long = a*42
+      hash = @attr.merge(:password=>long, password_confirmation=>long)
+      User.new(hash).should_not be_valid
+      #User.new(@attr.merge(:password=>short, password_confirmation=>short)).should_not be_valid      
+    end
+  end
 
+  describe "user encryption" do
+    before(:each) do
+      @user=User.create!(@attr)
+    end
 
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+
+    it "should set the encrypted password" do
+      @user.encrypted_password.should_not be_blank
+    end
+
+  end
+
+  describe "password encryption" do
+
+    before(:each) do
+      @user=User.create!(@attr) 
+    end
+
+    describe "has_password? method" do
+
+      it "should be true if the passwords match" do
+          @user.has_password?(@attr[:password]).should be_true
+      end
+      it "should be false if the passwords do NOT match" do
+          @user.has_password?("Invalid").should be_false
+      end
+
+    end
+  end
 end
 # == Schema Information
 #
 # Table name: users
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
