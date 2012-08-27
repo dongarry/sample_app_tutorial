dad4c5bb3eae61440c1dda88c8952aea6642f70e
Don <don@don-ThinkPad-T410>
2012-08-27 18:02:33 +0100

    chpt 8 - Form signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   
   def new
+  	@user = User.new
   	@title = "Sign up"
   end
 
@@ -9,4 +10,14 @@ class UsersController < ApplicationController
   	@title = @user.name	
   end
   
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		#Handle a successful save
+  	else
+  		@title ="Sign Up"
+  		render 'new'
+  	end
+  end
+
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -37,4 +37,43 @@ describe UsersController do
 
   end
 
+  #Chpt 8
+  describe "GET 'new' " do
+
+    it "should be successful" do
+      get :new
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get :new
+      response.should have_selector("title", :content=>"Sign up")
+    end
+  
+  end
+
+  describe "POST 'create'" do
+    describe "failure" do
+      before (:each) do
+        @attr ={:name=>"",:email=>"",:password=>"",:password_confirmation=>""}
+      end
+
+      it "should not create a user" do
+        lambda do 
+          post :create, :user =>@attr
+        end.should_not change(User,:count)
+      end
+
+      it "should have the right title" do
+        post :create, :user=>@attr
+        response.should have_selector("title",:content=>"Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+
+    end
+  end
 end
